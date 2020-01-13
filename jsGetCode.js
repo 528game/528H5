db2d5f84fb4ea489f23a26029bf91eca5dba5a94
528game <56824153+528game@users.noreply.github.com>
2020-01-13 21:23:21 +0800

    Update jsGetCode.js

--- jsGetCode.js
+++ jsGetCode.js
@@ -4,12 +4,12 @@ OnGetCode = function ( nType )
     //public
     if( nType == 0 )
     {
-        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3BiXDqYu+FYMv57Vn21P6yHJwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=";
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3Binjhk6K2uRjH/KSK0cAaRDwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=";
     }
     //Dev
     else
     {
-        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3BiXDqYu+FYMv57Vn21P6yHJwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=";
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3Binjhk6K2uRjH/KSK0cAaRDwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=";
     }
     return  strCode;
 }
