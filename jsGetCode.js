da2c92f00ecfae121d3112075b8fa0c97870222d
528game <56824153+528game@users.noreply.github.com>
2020-01-17 21:11:25 +0800

    Update jsGetCode.js

--- jsGetCode.js
+++ jsGetCode.js
@@ -4,7 +4,7 @@ OnGetCode = function ( nType )
     //public
     if( nType == 0 )
     {
-        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3Binjhk6K2uRjH/KSK0cAaRDwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3Binjhk6K2uRjH/KSK0cAaRDwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=";
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3Binjhk6K2uRjH/KSK0cAaRDwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=";
     }
     //Dev
     else
