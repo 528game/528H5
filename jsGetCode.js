77e2fe9506aab7176d35cac59ff39681117180b6
528game <56824153+528game@users.noreply.github.com>
2020-01-17 21:10:39 +0800

    Update jsGetCode.js

--- jsGetCode.js
+++ jsGetCode.js
@@ -4,12 +4,12 @@ OnGetCode = function ( nType )
     //public
     if( nType == 0 )
     {
-        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUxtI8s4GUeiangPb6cQTxCUmCHwiPDWsyuRRiEnz494a6zjN3wnaGI0/WCiia2yHw766ZDdUoOMkgJL8xDNEWWRcFvsS1RcjngVXYFq7q09BlUGj1WFf/ccUwwTnVQitgQ=";
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3Binjhk6K2uRjH/KSK0cAaRDwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3Binjhk6K2uRjH/KSK0cAaRDwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=";
     }
     //Dev
     else
     {
-        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUxtI8s4GUeiangPb6cQTxCUmCHwiPDWsyuRRiEnz494a6zjN3wnaGI0/WCiia2yHw766ZDdUoOMkgJL8xDNEWWRcFvsS1RcjngVXYFq7q09BlUGj1WFf/ccUwwTnVQitgQ=";
+        strCode = "8i5eSjuy/Md0hs80UnO3szTSNbBAG4MPUY+JczWlOUyFU6j864hJLjMhCinT8v6xWQ3b1l9TvfPMTaXxIaCWyBrBP/qhBH/qJTFitihy3Binjhk6K2uRjH/KSK0cAaRDwP+FXVictozcBvHJrDWleI6OrVAvF8jH83R7nukWAyM=";
     }
     return  strCode;
 }
